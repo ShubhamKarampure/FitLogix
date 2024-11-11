b13b0ef26ee05d8e1fc3a8bb596aec50b3642d9f
Shubham <shubhamkarampure26@gmail.com>
2024-11-11 16:58:33 +0530

    mongodb uri added

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
 

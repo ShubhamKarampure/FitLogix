1b765e46b3594d59f10569a13827cad7849d43aa
Shubham <shubhamkarampure26@gmail.com>
2024-11-11 16:22:46 +0530

    new-port

--- backend/index.js
+++ backend/index.js
@@ -86,13 +86,6 @@ wss.on('connection', (ws) => {
 });
 
 
-
-
-// Integrate WebSocket with the Express server
-app.server = app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
-
 app.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });

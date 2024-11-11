956592a301da930cbc5d03e4090cae5e6818ee0c
Shubham <shubhamkarampure26@gmail.com>
2024-11-11 16:11:08 +0530

    added oigin response

--- backend/index.js
+++ backend/index.js
@@ -93,6 +93,10 @@ app.server = app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Server is running' });
+});
+
 // Handling WebSocket upgrade requests
 app.server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
